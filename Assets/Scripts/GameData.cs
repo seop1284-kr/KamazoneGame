8ed1916a47b97191e05ab61000bc2da173c0fac5
seop <seop1284@naver.com>
2021-01-01 23:57:35 +0900

    add JsonFx, add stage_data

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using JsonFx.Json;
+using System.IO;
+
 public class GameData : Singleton<GameData> {
     public class Profile {
         
     }
 
-    private Stage[] stages;
+    public Stage[] stages;
 
+    // 스테이지 데이터 가져오기
+    public void LoadStageData() {
+        string fileName = "stage_data";
+        TextAsset textAsset = Resources.Load(fileName, typeof(TextAsset)) as TextAsset;
 
+        if (textAsset == null) { Debug.LogError(fileName + " is not found"); }
+        else { stages = JsonReader.Deserialize<Stage[]>(textAsset.text); }
+    }
 }
-
 
 public enum Type {
     BATTLE,
@@ -20,3 +29,4 @@ public enum Type {
     START,
     BOSS
 }
+
--- Assets/Scripts/MapScene.cs
+++ Assets/Scripts/MapScene.cs
@@ -7,6 +7,9 @@ public class MapScene : MonoBehaviour {
     [SerializeField] private Step[] steps;
 
     private void Start() {
+        GameData.Instance.LoadStageData();
+        Debug.Log(GameData.Instance.stages[0].name);
+        Debug.Log(GameData.Instance.stages[0].levels[0].type);
     } 
     
 }
